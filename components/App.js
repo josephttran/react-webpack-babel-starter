70f07d7a86c341ad0932bdd3f9ff4c4f576dbda5
JosephTTran <42635012+JosephTTran@users.noreply.github.com>
2018-09-13 05:12:22 -0700

    add html js files

---
+++ components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+          <h1>Hello</h1>
+          <p>React App</p>
+      </div>
+    );
+  }
+}
+
+export default App;
